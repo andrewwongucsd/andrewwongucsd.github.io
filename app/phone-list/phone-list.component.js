5faca6a8b32236033c8246f548bc153e0a2c9e2b
Andrew Wong <andrewwongucsd@alumni.ucsd.edu>
2016-07-24 20:38:51 -0700

    2016-07-24+20:38:51

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -5,18 +5,10 @@ angular.
   module('phoneList').
   component('phoneList', {
     templateUrl: 'app/phone-list/phone-list.template.html',
-    controller: function PhoneListController() {
-      this.phones = [
-        {
-          name: 'Nexus S',
-          snippet: 'Fast just got faster with Nexus S.'
-        }, {
-          name: 'Motorola XOOM™ with Wi-Fi',
-          snippet: 'The Next, Next Generation tablet.'
-        }, {
-          name: 'MOTOROLA XOOM™',
-          snippet: 'The Next, Next Generation tablet.'
-        }
-      ];
-    }
+    controller: ['$http',function PhoneListController($http) {
+      var self = this;
+      $http.get('data/phones.json').then(function(response){
+        self.phones = response.data;
+      });
+    }]
   });
