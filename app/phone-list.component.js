26d1f0c30f1f090f2e1be833fd7457cb942afdc7
Andrew Wong <andrewwongucsd@alumni.ucsd.edu>
2016-07-24 19:18:18 -0700

    2016-07-24+19:18:18

--- app/phone-list.component.js
+++ app/phone-list.component.js
@@ -1,22 +1,28 @@
-angular.module('phonecatApp').component('phoneList', {
-  template: '<ul>' +
+'use strict';
+
+// Register `phoneList` component, along with its associated controller and template
+angular.
+  module('phonecatApp').
+  component('phoneList', {
+    template:
+        '<ul>' +
           '<li ng-repeat="phone in $ctrl.phones">' +
             '<span>{{phone.name}}</span>' +
             '<p>{{phone.snippet}}</p>' +
           '</li>' +
         '</ul>',
-  controller: function PhoneListController(){
-    this.phone = [
-      {
-        name: 'Nexus S',
-        snippet: 'Fast just got faster with Nexus S.'
-      }, {
-        name: 'Motorola XOOM™ with Wi-Fi',
-        snippet: 'The Next, Next Generation tablet.'
-      }, {
-        name: 'MOTOROLA XOOM™',
-        snippet: 'The Next, Next Generation tablet.'
-      }
-    ];
-  }
-});
+    controller: function PhoneListController() {
+      this.phones = [
+        {
+          name: 'Nexus S',
+          snippet: 'Fast just got faster with Nexus S.'
+        }, {
+          name: 'Motorola XOOM™ with Wi-Fi',
+          snippet: 'The Next, Next Generation tablet.'
+        }, {
+          name: 'MOTOROLA XOOM™',
+          snippet: 'The Next, Next Generation tablet.'
+        }
+      ];
+    }
+  });
