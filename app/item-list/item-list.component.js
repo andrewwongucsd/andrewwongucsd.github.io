d0d03655e9dd0432289c3bab94d19094aa1a6d13
Andrew Wong <andrewwongucsd@alumni.ucsd.edu>
2016-07-27 20:11:37 -0700

    2016-07-27+20:11:37

--- app/item-list/item-list.component.js
+++ app/item-list/item-list.component.js
@@ -5,7 +5,7 @@ angular.
   module('itemList').
   component('itemList', {
     templateUrl: 'app/item-list/item-list.template.html',
-    controller: ['$http',function PhoneListController($http) {
+    controller: ['$http',function ItemListController($http) {
       var self = this;
       $http.get('data/andrewwong.json').then(function(response){
         self.items = response.data;
