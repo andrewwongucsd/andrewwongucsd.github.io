20ed6d2f487d236195d067b2b9390b02cfe689ef
Andrew Wong <andrewwongucsd@alumni.ucsd.edu>
2016-07-31 00:18:13 -0700

    2016-07-31+00:18:13

--- app/app.config.js
+++ app/app.config.js
@@ -19,5 +19,4 @@ angular.
     $mdThemingProvider.theme('dark-orange').backgroundPalette('orange').dark();
     $mdThemingProvider.theme('dark-purple').backgroundPalette('deep-purple').dark();
     $mdThemingProvider.theme('dark-blue').backgroundPalette('blue').dark();
-  }
-]);
+  });
