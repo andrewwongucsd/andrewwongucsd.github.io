ca3f7e6763c85d50f59e8cfc0e5ed8dbe0c31d96
Andrew Wong <andrewwongucsd@alumni.ucsd.edu>
2016-09-09 12:54:58 -0700

    2016-09-09+12:54:58

--- app/item-list-print/item-list-print.component.js
+++ app/item-list-print/item-list-print.component.js
@@ -8,5 +8,10 @@ angular.
       $http.get('data/andrewwong.json').then(function(response){
         self.items = response.data;
       });
+      self.select = function(){
+        var r = document.getElementsByClassName("radio");
+        console.log(r);
+        alert(r);
+      };
     }]
   });
