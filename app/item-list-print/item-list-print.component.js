0e9a1d51ba52514de0f0d90f8bef75d469fcedde
Andrew Wong <andrewwongucsd@alumni.ucsd.edu>
2016-09-09 13:01:17 -0700

    2016-09-09+13:01:17

--- app/item-list-print/item-list-print.component.js
+++ app/item-list-print/item-list-print.component.js
@@ -5,13 +5,13 @@ angular.
     templateUrl: 'app/item-list-print/item-list-print.template.html',
     controller: ['$http',function ItemListController($http) {
       var self = this;
+      self.select = function select(){
+        var r = document.getElementsByClassName("radio");
+        console.log(r);
+        alert(r);
+      };
       $http.get('data/andrewwong.json').then(function(response){
         self.items = response.data;
-        self.select = function select(){
-          var r = document.getElementsByClassName("radio");
-          console.log(r);
-          alert(r);
-        };
       });
     }]
   });
