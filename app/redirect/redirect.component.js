b29565916bf1d1de06e49dc81dfb88dba921d872
Andrew Wong <andrewwongucsd@alumni.ucsd.edu>
2016-09-14 23:43:19 -0700

    2016-09-14+23:43:19

--- app/redirect/redirect.component.js
+++ app/redirect/redirect.component.js
@@ -4,7 +4,7 @@ angular.
   component('redirect', {
     templateUrl: 'app/redirect/redirect.template.html',
     controller: ['$http',function RedirectController($http) {
-      $window.location.href = "https://github.com/andrewwongucsd";
-      console.log('redirecing');
+      window.location.href = "https://github.com/andrewwongucsd";
+      console.log('redirecting');
     }]
   });
